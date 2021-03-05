3d1e0a1edc8c64ddbd2488c9b6545bc8bb0c5ba4
mszhang <mszhang02@gmail.com>
2021-03-04 18:58:16 -0800

    fixed spacing

--- multisummoner.py
+++ multisummoner.py
@@ -27,12 +27,15 @@ class MultiSummoner:
         s = ""
         for summoner in self.summoner_list:
             s += str(summoner) + '\n'
+        
         rows = len(self.modes) + 6
         colsep = ' | '
+
         col1 = ['-'*36, '{:<16s}{:>8s}{:>12s}'.format('Mode', 'Games', 'Seconds'), '-'*36]
         for key in self.modes:
             col1.append(str(self.modes[key]))
         col1 += ['-'*36, '{:<16s}{:>8d}{:>12d}'.format('total (' + str(len(self.summoner_list)) + ' accts)', self.num_games, self.total_time), '-'*36]
+
         col2 = ['-'*36, '{:<16s}{:>8s}{:>12s}'.format('Played With', 'Games', 'Seconds'), '-'*36]
         for i in range(rows-4):
             if i >= len(self.played_with_list):
@@ -40,18 +43,22 @@ class MultiSummoner:
             else:
                 col2.append('{:<16s}{:>8d}{:>12d}'.format(get_name_by_summoner_id(self.server, self.played_with_list[i][2]), self.played_with_list[i][0], self.played_with_list[i][1]))
         col2.append('-'*36)
+
         for i in range(rows):
             s += col1[i] + colsep + col2[i]
             if i != rows - 1:
                 s += '\n'
+
         return s
 
     def summoner_update(self, summoner):
         self.num_games += summoner.num_games
         self.total_time += summoner.total_time
+
         for key in self.modes:
             self.modes[key].num_games += summoner.modes[key].num_games
             self.modes[key].total_time += summoner.modes[key].total_time
+        
         for summoner_id in summoner.played_with:
             if summoner_id not in self.played_with:
                 self.played_with[summoner_id] = [0, 0]
@@ -62,6 +69,7 @@ class MultiSummoner:
         for summoner in self.summoner_list:
             summoner.calculate()
             self.summoner_update(summoner)
+        
         for key in self.played_with:
             self.played_with_list.append((self.played_with[key][0], self.played_with[key][1], key))
             self.played_with_list.sort(reverse=True)
--- summoner.py
+++ summoner.py
@@ -28,10 +28,12 @@ class Summoner:
     def __str__(self):
         rows = len(self.modes) + 6
         colsep = ' | '
+
         col1 = ['-'*36, '{:<16s}{:>8s}{:>12s}'.format('Mode', 'Games', 'Seconds'), '-'*36]
         for key in self.modes:
             col1.append(str(self.modes[key]))
         col1 += ['-'*36, '{:<16s}{:>8d}{:>12d}'.format(self.name, self.num_games, self.total_time), '-'*36]
+
         col2 = ['-'*36, '{:<16s}{:>8s}{:>12s}'.format('Played With', 'Games', 'Seconds'), '-'*36]
         for i in range(rows-4):
             if i >= len(self.played_with_list):
@@ -39,19 +41,23 @@ class Summoner:
             else:
                 col2.append('{:<16s}{:>8d}{:>12d}'.format(get_name_by_summoner_id(self.server, self.played_with_list[i][2]), self.played_with_list[i][0], self.played_with_list[i][1]))
         col2.append('-'*36)
+
         s = ""
         for i in range(rows):
             s += col1[i] + colsep + col2[i]
             if i != rows - 1:
                 s += '\n'
+
         return s
     
     def match_update(self, match):
         self.modes[match['queueId']].match_update(match)
         self.num_games += 1
         self.total_time += match['gameDuration']
+
         part_id_to_summ_id = {participant_identity['participantId']: participant_identity['player']['summonerId'] for participant_identity in match['participantIdentities']}
         summ_id_to_side = {part_id_to_summ_id[participant['participantId']]: participant['teamId'] for participant in match['participants']}
+
         for summoner_id in summ_id_to_side:
             if summoner_id != self.summoner_id and summ_id_to_side[summoner_id] == summ_id_to_side[self.summoner_id]:
                 if summoner_id not in self.played_with:
@@ -64,6 +70,7 @@ class Summoner:
         for i in tqdm(range(len(matchlist))):
             match = get_match_by_id(self.server, matchlist[i]['gameId'])
             self.match_update(match)
+            
         for key in self.played_with:
             self.played_with_list.append((self.played_with[key][0], self.played_with[key][1], key))
             self.played_with_list.sort(reverse=True)
